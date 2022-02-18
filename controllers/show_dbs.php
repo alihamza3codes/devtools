665046769882c1be9d7bc4bfbf3eca1d046a8bd2
alihamza3codes <97583834+alihamza3codes@users.noreply.github.com>
2022-02-18 16:38:00 +0500

    Delete controllers directory

--- controllers/show_dbs.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../2.includes/inc/database.php");
-//  show data base names 
-if ($_POST['type'] == "show-db-name") {
-    $db_names = $db->query("SHOW DATABASES");
-    if ($db_names) {
-        print_r($db_names);
-    }
-}
